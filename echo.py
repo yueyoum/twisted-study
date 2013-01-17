8fc37536b5396180c7a1f0fef7e12f62b4eb8d4c
yueyoum <yueyoum@gmail.com>
2013-01-18 00:19:22 +0800

    adjust and add commit

--- echo.py
+++ echo.py
@@ -1,14 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from twisted.internet import reactor, protocol, endpoints
 
 class UpperProtocol(protocol.Protocol):
     def connectionMade(self):
-        self.transport.write("hi, welcome\n")
         self.factory.amount +=1 
-        print 'client amount =', self.factory.amount
+        self.transport.write("hi, there are %d clients\n" % self.factory.amount)
 
     def connectionLost(self, reason):
         self.factory.amount -= 1
-        print 'client amount =', self.factory.amount
 
     def dataReceived(self, data):
         self.transport.write(data.upper())
@@ -17,11 +17,15 @@ class UpperProtocol(protocol.Protocol):
 
 
 class MyFactory(protocol.ServerFactory):
+    protocol = UpperProtocol
     amount = 0
 
 
-factory = MyFactory()
-factory.protocol = UpperProtocol
-
-endpoints.serverFromString(reactor, "tcp:8080").listen(factory)
+endpoints.serverFromString(reactor, "tcp:8080").listen(MyFactory())
 reactor.run()
+
+# reactor 会一直循环，直到关闭socket连接
+# reactor 使用factory作用于连接
+# factory 为每一个client 建立一个protocol 实例
+# factory 或者 protocol 通过实例，或者实现类的方法来添加功能
+
